8ae4157ff59d920ae2eae1104c828f318e5ca591
YufkaBoi <116082464+YufkaBoi@users.noreply.github.com>
2023-06-27 11:12:50 +0200

    Update index.php as main file

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-  echo "test";
-?>
+<html>
+  <head>
+    <title>- YufkaBoi -</title>
+  </head>
+  <body>
+    <?php 
+      echo "hello";
+    ?>
+    <div style="text-align: center">
+      <h3>- [YufkaBoi] -</h3>
+      <p>Soc: Axolordl</p>
+    </div>
+  </body>
+</html>
